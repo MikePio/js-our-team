d8682419810447c1632018797400541c48f02d81
MikePio <michelepiopilla@gmail.com>
2023-04-03 17:19:53 +0200

    Created the object array

--- script.js
+++ script.js
@@ -1,3 +1,69 @@
+/*
+Utilizzando i dati forniti, creare un array di oggetti per rappresentare i membri del team.
+Ogni membro è caratterizzato dalle seguenti informazioni: nome, ruolo e foto.
+
+MILESTONE 0:
+Creare l’array di oggetti con le informazioni fornite.
+
+MILESTONE 1:
+Stampare su console, per ogni membro del team, le informazioni di nome, ruolo e la stringa della foto.
+Utilizzate liberamente il tipo di ciclo che preferite (for, for in o for of)
+
+MILESTONE 2:
+Stampare le stesse informazioni su DOM sottoforma di stringhe
+
+BONUS 1:
+Trasformare la stringa foto in una immagine effettiva
+
+BONUS 2:
+Organizzare i singoli membri in card/schede
+*/
+
+/*
+
+
+
+Dati:
+Wayne Barnett   Founder & CEO         wayne-barnett-founder-ceo.jpg
+Angela Caroll   Chief Editor          angela-caroll-chief-editor.jpg
+Walter Gordon   Office Manager        walter-gordon-office-manager.jpg9
+Angela Lopez    Social Media Manager  angela-lopez-social-media-manager.jpg
+Scott Estrada   Developer             scott-estrada-developer.jpg
+Barbara Ramos   Graphic Designer      barbara-ramos-graphic-designer.jpg
+*/
+
+const ourTeam = [
+  {
+    name: 'Wayne Barnett',
+    rule: 'Founder & CEO',
+    photo: 'wayne-barnett-founder-ceo.jpg'
+  },
+  {
+    name: 'Angela Caroll',
+    rule: 'Chief Editor ang',
+    photo: 'angela-caroll-chief-editor.jpg'
+  },
+  {
+    name: 'Walter Gordon',
+    rule: 'Office Manager',
+    photo: 'walter-gordon-office-manager.jpg9'
+  },
+  {
+    name: 'Angela Lopez' ,
+    rule: 'Social Media Manager',
+    photo: 'angela-lopez-social-media-manager.jpg'
+  },
+  {
+    name: 'Scott Estrada',
+    rule: 'Developer',
+    photo: 'scott-estrada-developer.jpg'
+  },
+  {
+    name: 'Barbara Ramos',
+    rule: 'Graphic Designer',
+    photo: 'barbara-ramos-graphic-designer.jpg'
+  }
+]
 
 
 
